15ab2a200ca8b3e917831082ccc62f72a0d658f0
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-07-07 23:11:48 -0300

    feat: theme toggle button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from '../shared/context';
 
 export function RoutesApp() {
+  const { toggleTheme } = useAppThemeContext();
   return(
     <Routes>
-      <Route path='main' element={<p> Initial Page </p>}></Route>
-      <Route path='*' element={<Navigate to='/main'/>}></Route>
+      <Route path='/main' element={<Button variant='contained' color='primary' onClick={toggleTheme}>Theme</Button>}/>
+      <Route path='*' element={<Navigate to='/main' /> } />
     </Routes>
   )
 }
